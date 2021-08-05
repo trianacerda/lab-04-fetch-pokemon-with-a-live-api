cdde61f284349d37fa9bac9f86cb1528b146f0f7
Triana Cerda <trianaRcerda@gmail.com>
2021-08-05 16:11:42 -0700

    added keys for pokemon

--- src/Pokeitem.js
+++ src/Pokeitem.js
@@ -5,8 +5,13 @@ class Pokeitem extends Component {
     const { pokemon } = this.props;
     return (
       <li>
-        <h3>{pokemon.pokemon}</h3>
-        <h3>{pokemon.pokemon}</h3>
+        <h3>pokemon: {pokemon.pokemon}</h3>
+        <h3>type 1: {pokemon.type_1}</h3>
+        <h3>type 2: {pokemon.type_2}</h3>
+        <h3>ability 1: {pokemon.ability_1}</h3>
+        <h3>ability 2: {pokemon.ability_2}</h3>
+        <h3>ability hidden: {pokemon.ability_hidden}</h3>
+        <h3>shape: {pokemon.shape}</h3>
       </li>
     );
   }
