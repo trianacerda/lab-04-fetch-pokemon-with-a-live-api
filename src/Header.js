eed8c1fa57b80b8180a2c427a81a372b685e33ff
Triana Cerda <trianaRcerda@gmail.com>
2021-08-09 15:00:19 -0700

    rendering pokemon on poke page + working details/home page

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 class Header extends Component {
   state = {};
   render() {
-    return <section></section>;
+    return (
+      <header>
+        <div className="links">
+          <NavLink exact to="/">
+            Home Page
+          </NavLink>
+          <NavLink to="/pokemon">All The Poke</NavLink>
+          <NavLink to="/pokemon/:id">Poke Details</NavLink>
+        </div>
+      </header>
+    );
   }
 }
 
